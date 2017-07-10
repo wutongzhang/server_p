60f486646c9fd35ec29cf148950f763468dcf7b6
wutongzhang <544635106@qq.com>
2017-07-10 10:43:18 +0800

    还未加入封装socket和守护进程

--- Agent2.py
+++ Agent2.py
@@ -1,3 +1,4 @@
+# coding=gbk
 import socket
 import sys
 import pymongo
@@ -27,31 +28,14 @@ def handle_req(conn, req):  # 处理前端请求
             "type": "list_all_shapes",
             "all_shapenames": list_all_shapes()
         }
-        fp = open('req_ALEX_20170708142032_list_all_shapes.json', 'w')
-        fp.write(str(response))
-
-        fp.close()
-        t = tarfile.open("req_ALEX_20170708142032_list_all_shapes.tar.gz", "w:gz")
-        t.add("req_ALEX_20170708142032_list_all_shapes.json")
-        t.close()
-        t = open('req_ALEX_20170708142032_list_all_shapes.tar.gz', 'rb')
-        while 1:
-            filedata = t.read(1024)
-            if not filedata: break
-            conn.sendall(filedata)
-            # send_j_response(conn,response)
+        filename = req['clientfilename']
+        send_j_response(conn,response,filename)
 
     elif req['type'] == 'list_doc_tree':  # 前端向后台发送请求，获取指定内层目录
         doc_tree = list_doc_tree(req['doc_tree_name'])
-        if req['ack'] == 1:
-            print(conn.send(doc_tree.encode('utf-8')))  # 后台向前端发送内层目录
-        else:
-            response = {
-                "type": "list_doc_tree",
-                "doc_tree_length": len(doc_tree),
-                "doc_tree_name": req['doc_tree_name']
-            }
-            send_j_response(conn, response)
+        doc_tree = eval(doc_tree)
+        filename = req['clientfilename']
+        send_j_response(conn, doc_tree, filename)
 
     elif req['type'] == 'log_in':  # 前端向后台发送请求，请求登录
         response = {
@@ -93,21 +77,83 @@ def handle_req(conn, req):  # 处理前端请求
 
     elif req['type'] == 'download_file':  # 前端向后台发送请求，获得指定文件
         new_filename = name_switch(req['file_name']['which_file'])
-        if req['ack'] == 1:
-            file = find_file(new_filename, req['file_name'])
-            print(conn.send(file))
-        else:
-            file_length, file_authority = get_file_information(new_filename, req['file_name'])
-            response = {
-                "type": "download_file",
-                "file_length": file_length,
-                "file_authority": file_authority,
-                "file_name": req['file_name']['which_file']
-            }
-            send_j_response(conn, response)
+        send_file(conn, find_file(new_filename, req['file_name'],req['clientfilename']))
+        # if req['ack'] == 1:
+            # file = find_file(new_filename, req['file_name'])
+            # print(conn.send(file))
+        # else:
+            # file_length, file_authority = get_file_information(new_filename, req['file_name'])
+            # response = {
+                # "type": "download_file",
+                # "file_length": file_length,
+                # "file_authority": file_authority,
+                # "file_name": req['file_name']['which_file']
+            # }
+            # send_j_response(conn, response)
     else:
         print('Wrong request!')
 
+
+def send_j_response(conn, res, filename):  # 向前台发送json压缩包
+    #将json写入文件
+    #filename = 'req_ALEX_20170708142032_list_all_shapes.json'
+    fp = open(filename, 'w')
+    fp.write(json.dumps(res))
+    fp.close()
+	
+    #将json文件打包压缩
+    filename = filename.replace(".json",".tar.gz")
+    t = tarfile.open(filename, "w:gz")
+    filename = filename.replace(".tar.gz",".json")
+    t.add(filename)
+    t.close()
+    filename = filename.replace(".json",".tar.gz")
+    s_filename = filename.encode("UTF-8")
+	
+    #向client端发送文件头，包括压缩包名称和大小
+   # msg = 'OK'
+    #conn.sendall(msg.encode('utf-8'))
+    fhead = struct.pack('<128s11I', s_filename, 0, 0, 0, 0, 0, 0, 0, 0, os.stat(filename).st_size, 0, 0)
+    conn.send(fhead)
+	
+    #向client端发送压缩包
+    t = open(filename, 'rb')
+    while 1:
+        filedata = t.read(1024)
+        if not filedata: break
+        conn.sendall(filedata)
+    t.close()
+    os.remove(filename)
+    filename = filename.replace(".tar.gz",".json")
+    os.remove(filename)
+    print(res)
+
+def send_file(conn, filename):
+	#将文件打包压缩
+    filename = filename.replace(".dat",".tar.gz")
+    t = tarfile.open(filename, "w:gz")
+    filename = filename.replace(".tar.gz",".dat")
+    t.add(filename)
+    t.close()
+    filename = filename.replace(".dat",".tar.gz")
+    s_filename = filename.encode("UTF-8")
+	
+    #向client端发送文件头，包括压缩包名称和大小
+    #msg = 'OK'
+    #conn.sendall(msg.encode('utf-8'))
+    fhead = struct.pack('<128s11I', s_filename, 0, 0, 0, 0, 0, 0, 0, 0, os.stat(filename).st_size, 0, 0)
+    conn.send(fhead)
+	
+    #向client端发送压缩包
+    t = open(filename, 'rb')
+    while 1:
+        filedata = t.read(1024)
+        if not filedata: break
+        conn.sendall(filedata)
+    t.close()
+    os.remove(filename)
+    filename = filename.replace(".tar.gz",".dat")
+    os.remove(filename)
 
 def name_switch(old_filename):
     if old_filename == "Input_ConfigForceMoment.dat":
@@ -139,7 +185,7 @@ def name_switch(old_filename):
     return new_filename
 
 
-def find_file(filename, obj):
+def find_file(filename, obj, clientfilename):#filename是文件在数据库里的名字，obj是前端发来的文件相关信息
     if filename == "statistics":
         item = db["STATISTICS"].find_one({"shape_name": obj[shape_name]})
         db_file_id = item[filename]["db_file_id"]
@@ -160,7 +206,10 @@ def find_file(filename, obj):
             db_file_id = item[filename]["db_file_id"]
 
     bytes_data = pyMongo.getFile(fs, db_file_id)
-    return bytes_data
+    fp = open(clientfilename,'wb')
+    fp.write(bytes_data)
+    fp.close()
+    return clientfilename
 
 
 def search_condition(obj):  # 构成查询条件字段
@@ -231,12 +280,6 @@ def get_file_information(filename, obj):
             file_authority = item[filename]["file_authority"]
 
     return file_size, file_authority
-
-
-def send_j_response(conn, res):  # 向前台发送json
-    j_res = json.dumps(res)
-    conn.sendall(str(j_res).encode('utf-8'))
-    print(res)
 
 
 def list_all_shapes():  # 查找所有外形名（未考虑没有数据的情况）
